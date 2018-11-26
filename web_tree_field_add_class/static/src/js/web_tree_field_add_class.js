ad82b94d162f9e51fae23305798f0dc32ae21b86
Bob Leers <bob@novacode.nl>
2018-11-26 12:07:10 +0100

    web_tree_field_add_class: Also add class on table header (th).

--- web_tree_field_add_class/static/src/js/web_tree_field_add_class.js
+++ web_tree_field_add_class/static/src/js/web_tree_field_add_class.js
@@ -6,38 +6,48 @@ odoo.define('web_tree_field_add_class', function (require) {
 
     ListRenderer.include({
         /**
-         * Add class attribute on a cell during it's render
+         * Add class attribute on a table td (cell) during it's render
          *
          * @override
          */
         _renderBodyCell: function (record, node, colIndex, options) {
             var $td = this._super.apply(this, arguments);
-            this.addClass($td, record, node);
+            this.addClass($td, node);
             return $td;
         },
 
         /**
-         * @param {Query Node} $td a <td> tag inside a table representing a list view
+         * Add class attribute on a table th (heading) during it's render
+         *
+         * @override
+         */
+        _renderHeaderCell: function (node) {
+            var $th = this._super.apply(this, arguments);
+            this.addClass($th, node);
+            return $th;
+        },
+
+        /**
+         * @param {Query Node} $el a tag (td,th) inside a table representing a list view
          * @param {Object} node an XML node (must be a <field>)
          */
-        addClass: function ($td, record, node, ctx) {
+        addClass: function ($el, node, ctx) {
             if (!node.attrs.options) { return; }
             if (node.tag !== 'field') { return; }
             var nodeOptions = node.attrs.options;
-            console.log(nodeOptions);
             if (!_.isObject(nodeOptions)) {
                 nodeOptions = pyeval.py_eval(nodeOptions);
             }
-            this.addClassHelper($td, nodeOptions, node, ctx);
+            this.addClassHelper($el, nodeOptions, node, ctx);
         },
 
         /**
          * @param {Object} nodeOptions a mapping of nodeOptions parameters
          * @param {Object} node an XML node (must be a <field>)
          */
-        addClassHelper: function ($td, nodeOptions, node, ctx) {
+        addClassHelper: function ($el, nodeOptions, node, ctx) {
             if (nodeOptions['add_class']) {
-                $td.addClass(nodeOptions['add_class']);
+                $el.addClass(nodeOptions['add_class']);
             }
         },
     });
